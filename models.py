ac41c67d1f352592fd4f9bf01c5e74b53a151662
Tomas Reimers <tomasreimers@gmail.com>
2015-01-29 09:27:20 -0800

    Added models

--- models.py
+++ models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Reading(models.Model):
+
+    VOTE_CHOICES = (
+        ('u', 'upvote'),
+        ('d', 'downvote'),
+        (NULL, 'undefined'),
+    )
+
+    user = models.ForeignKey(User)
+    bookId = models.CharField(max_length=10)
+    isRead = models.BooleanField()
+    vote = models.ChatField(max_length=1, choices=VOTE_CHOICES)
